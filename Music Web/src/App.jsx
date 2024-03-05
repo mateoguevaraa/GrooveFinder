18021d61076d7aceb96f4b95e1922fd8784aec8c
mateoguevaraa <mateoaguevara@gmail.com>
2024-03-05 02:43:44 -0300

    Listo el fetching, falta la ui

--- Music Web/src/App.jsx
+++ Music Web/src/App.jsx
@@ -1,63 +1,36 @@
 import { useState, useEffect } from "react";
-import { genres } from "./data/music-genres.json";
+import { useTracks } from "./services/musicFetch";
+// import { Card } from "./components/trackCard";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./style.css";
 
-const clientIdSpotify = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
-const clientSecretIdSpotify = import.meta.env.VITE_SPOTIFY_SECRET_CLIENT_ID;
-
 function App() {
-  const [genre, setGenre] = useState();
-  const [artists, setArtists] = useState();
-  const [randomIndex, setRandomIndex] = useState();
-
-  const handleGenreGenerator = () => {
-    let randomNumber;
-    do {
-      randomNumber = Math.floor(Math.random() * genres.length);
-    } while (randomNumber === randomIndex);
-
-    const genreRandom = genres[randomNumber].name;
-    setGenre(genreRandom);
-    setRandomIndex(randomNumber);
-    searchArtistByGenre(genreRandom);
-  };
-
-  async function searchArtistByGenre(genre) {
-    const offset = Math.floor(Math.random() * 50);
-    const response = await fetch(
-      `https://api.spotify.com/v1/search?q=genre%3A${genre}&type=track&offset=${offset}`,
-      {
-        headers: {
-          Authorization: "Bearer " + (await getAccessToken()),
-          "Content-Type": "application/json",
-        },
-      }
-    );
-    const data = await response.json();
-    console.log(data.tracks.items);
-    const artistsFetch = data.tracks.items;
-    setArtists(artistsFetch);
-  }
-
-  async function getAccessToken() {
-    const response = await fetch("https://accounts.spotify.com/api/token", {
-      method: "POST",
-      headers: {
-        Authorization:
-          "Basic " + btoa(clientIdSpotify + ":" + clientSecretIdSpotify),
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-      body: "grant_type=client_credentials",
-    });
-    const data = await response.json();
-    return data.access_token;
-  }
+  const { tracks, handleGenreGenerator, genre } = useTracks();
 
   return (
     <main>
       <h1>Genre Discover</h1>
-      <button onClick={handleGenreGenerator}>Discover a new genre</button>
+      <button onClick={handleGenreGenerator} className="btn btn-primary">
+        Discover a new genre
+      </button>
       {genre ? <p>{genre}</p> : null}
+      <section className="tracks-display">
+        {tracks.map(function (track) {
+          return (
+            <div key={track.id} className="tracks card">
+              <img
+                src={track.album.images[1].url}
+                alt={`${track.artists.name} cover`}
+                className="card-img-top"
+              />
+              <div className="card-body">
+                <strong className="card-title">{track.name}</strong>
+                <p>{track.artists[0].name}</p>
+              </div>
+            </div>
+          );
+        })}
+      </section>
     </main>
   );
 }
--- Music Web/src/services/musicFetch.js
+++ Music Web/src/services/musicFetch.js
@@ -1,8 +1,41 @@
+import { useState } from "react";
+import { genres } from "../data/music-genres.json";
+
 const clientIdSpotify = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
 const clientSecretIdSpotify = import.meta.env.VITE_SPOTIFY_SECRET_CLIENT_ID;
 
-export function useMusic(genre) {
+export function useTracks() {
   const [tracks, setTracks] = useState([]);
+  const [randomIndex, setRandomIndex] = useState();
+  const [genre, setGenre] = useState();
+
+  async function searchArtistByGenre(genre) {
+    const offset = Math.floor(Math.random() * 20);
+    const response = await fetch(
+      `https://api.spotify.com/v1/search?q=genre%3A${genre}&type=track&offset=${offset}&limit=30`,
+      {
+        headers: {
+          Authorization: "Bearer " + (await getAccessToken()),
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+    const tracksFetch = data.tracks.items;
+    setTracks(tracksFetch);
+  }
+
+  const handleGenreGenerator = () => {
+    let randomNumber;
+    do {
+      randomNumber = Math.floor(Math.random() * genres.length);
+    } while (randomNumber === randomIndex);
+
+    const genreRandom = genres[randomNumber].name;
+    setGenre(genreRandom);
+    setRandomIndex(randomNumber);
+    searchArtistByGenre(genreRandom);
+  };
 
   async function getAccessToken() {
     const response = await fetch("https://accounts.spotify.com/api/token", {
@@ -18,52 +51,5 @@ export function useMusic(genre) {
     return data.access_token;
   }
 
-  // Acá busco artistas por el género generado
-  async function searchArtistByGenre(genre) {
-    const response = await fetch(
-      `https://api.spotify.com/v1/search?q=${encodeURIComponent(
-        genre
-      )}&type=artist`,
-      {
-        headers: {
-          Authorization: "Bearer " + (await getAccessToken()),
-          "Content-Type": "application/json",
-        },
-      }
-    );
-    const data = await response.json();
-    console.log(data);
-    return data.artists.items;
-  }
-
-  // Función para obtener canciones de un artista
-  async function getSongsFromArtist(artistId) {
-    const response = await fetch(
-      `https://api.spotify.com/v1/artists/${artistId}/albums`,
-      {
-        headers: {
-          Authorization: "Bearer " + (await getAccessToken()),
-        },
-      }
-    );
-    const data = await response.json();
-    const albums = data.items;
-
-    // Recorrer todos los álbumes y obtener las canciones
-    let songs = [];
-    for (const album of albums) {
-      const albumResponse = await fetch(
-        `https://api.spotify.com/v1/albums/${album.id}/tracks`,
-        {
-          headers: {
-            Authorization: "Bearer YOUR_ACCESS_TOKEN", // Reemplaza YOUR_ACCESS_TOKEN con tu token de acceso de Spotify
-          },
-        }
-      );
-      const albumData = await albumResponse.json();
-      songs = [...songs, ...albumData.items];
-    }
-
-    return songs;
-  }
+  return { tracks, handleGenreGenerator, genre };
 }
