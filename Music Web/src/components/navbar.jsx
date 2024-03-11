a8e7dfdfb153e83e166e6b5fb174ece783912242
mateoguevaraa <mateoaguevara@gmail.com>
2024-03-11 00:13:08 -0300

    listo el star button, terminado

--- Music Web/src/components/navbar.jsx
+++ Music Web/src/components/navbar.jsx
@@ -5,6 +5,20 @@ export function NavBar() {
         <a className="navbar-brand" href="">
           GrooveFinder
         </a>
+        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+          <li className="nav-item">
+            <a
+              className="nav-link"
+              href="https://github.com/mateoguevaraa/GrooveFinder"
+              target="blank"
+            >
+              <button className="btn btn-sm github-btn">
+                {" "}
+                <i className="fa-regular fa-star"></i>Star
+              </button>
+            </a>
+          </li>
+        </ul>
       </div>
     </nav>
   );
